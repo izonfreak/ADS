25f15b5780329276c0f55d7ed4258d399796f11e
juarezr <ricardo.juarez@mvtec.com>
2026-07-06 11:47:09 +0200

    Use std::runtime_error instead of std::exception
    Fix clang-tidy ERR60-CPP rule "Exception objects must be nothrow copy
    constructable".
    
    std::exception is the abstract base class and does not have a
    constructor that accepts a string. Since you inherit from it the storage
    of the string message for what() needs to be allocated in the
    constructor of AdsException in this case using a std::string. Allocating
    a std::string can theoretically throw (e.g. bad_alloc). If your
    exception is copy more than once during stack unwinding you will not be
    able to catch anything.
    
    To solve this simply use std::runtime_error which comes with string
    constructors and noexcept copy constructors.
    
    Note here that errorCode should not be a public member of the exception
    as one also can theoretically, throw the const away and modify the
    value which is not something you want. However, this is leave "as is"
    for compatibility reasons.

--- AdsLib/AdsException.h
+++ AdsLib/AdsException.h
@@ -8,25 +8,13 @@
 #include <stdexcept>
 #include <string>
 
-struct AdsException : std::exception {
-	AdsException(const long adsErrorCode)
-		: errorCode(adsErrorCode)
-		, m_Message("Ads operation failed with error code " +
-			    std::to_string(adsErrorCode) + ".")
+struct AdsException : std::runtime_error {
+	explicit AdsException(const long adsErrorCode)
+		: std::runtime_error("Ads operation failed with error code " +
+		                     std::to_string(adsErrorCode) + "."),
+		  errorCode(adsErrorCode)
 	{
-	}
-
-	virtual ~AdsException() throw()
-	{
-	}
-
-	virtual const char *what() const throw()
-	{
-		return m_Message.c_str();
 	}
 
 	const long errorCode;
-
-    protected:
-	const std::string m_Message;
 };
